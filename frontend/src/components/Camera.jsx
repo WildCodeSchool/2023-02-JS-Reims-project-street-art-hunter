c5c92272d823288a8511fadf7f3786400959d82b
Carenjot <carenjot.aurelien@hotmail.fr>
2023-05-31 15:03:08 +0200

    Use of react-geolocated and first test

--- frontend/src/components/Camera.jsx
+++ frontend/src/components/Camera.jsx
@@ -1,9 +1,18 @@
+import { useGeolocated } from "react-geolocated";
 import Webcam from "react-webcam";
 import { useRef, useState, useCallback } from "react";
 
 function Camera() {
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
+
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } =
+    useGeolocated({
+      positionOptions: {
+        enableHighAccuracy: false,
+      },
+      userDecisionTimeout: 5000,
+    });
 
   const capture = useCallback(() => {
     setImgSrc(webcamRef.current.getScreenshot());
@@ -20,6 +29,7 @@ function Camera() {
           <div className="img-container">
             <img src={imgSrc} alt="webcam" />
           </div>
+
           <div className="btn-container">
             <button type="button" onClick={retake}>
               Reprendre la photo
@@ -41,6 +51,12 @@ function Camera() {
               Capture photo
             </button>
           </div>
+        </>
+      )}
+      {isGeolocationAvailable && isGeolocationEnabled && coords && imgSrc && (
+        <>
+          <h1>Latitude : {coords.latitude}</h1>
+          <h1>Longitude : {coords.longitude}</h1>
         </>
       )}
     </div>
