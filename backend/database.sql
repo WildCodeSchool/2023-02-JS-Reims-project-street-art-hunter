5347eeb3f6b23dcb97d2554caf31c621a4094715
FlavienJovic <flavien.jovic@gmail.com>
2023-06-05 14:54:34 +0200

    ajouts de street arts dans la bd

--- backend/database.sql
+++ backend/database.sql
@@ -13,4 +13,11 @@ CREATE TABLE gallery (
   creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   image BLOB NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-insert into street_art (image, longitude, latitude, is_valid) VALUES ('/assets/images/rue_libergier.jpg',49.26538,4.01434, true)
+
+insert into street_art (image, longitude, latitude, is_valid) VALUES ('/assets/images/rue_libergier.jpg',49.26538,4.01434, true);
+insert into street_art (image, longitude, latitude, is_valid) VALUES ('/assets/images/avenue_paul_marchandeau.jpg',49.24220,4.02503, true);
+insert into street_art (image, longitude, latitude, is_valid) VALUES ('/assets/images/boulevard_wilson_1.jpg',49.24043,4.02034, true);
+insert into street_art (image, longitude, latitude, is_valid) VALUES ('/assets/images/boulevard_wilson_2.jpg',49.24098,4.01945, true);
+insert into street_art (image, longitude, latitude, is_valid) VALUES ('/assets/images/place_du_forum.jpg',49.2567,4.03466, true);
+insert into street_art (image, longitude, latitude, is_valid) VALUES ('/assets/images/rue_de_courcelles.jpg',49.26529,4.01402, true)
+
