9171fe31f923f3db318a25e2228ba13693ab9d76
OkamiAka <nils.caqueux@gmail.com>
2023-06-06 12:57:05 +0200

    correction

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,16 +1,19 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import logo from "../assets/logo.png";
 
 export default function Home() {
+  const navigate = useNavigate();
   return (
     <div className="home">
       <img src={logo} alt="" />
-      <Link to="/menu">
-        <button type="button" className="return">
-          Start
-        </button>
-      </Link>
+      <button
+        type="button"
+        className="return"
+        onClick={() => navigate("/menu")}
+      >
+        Start
+      </button>
     </div>
   );
 }
