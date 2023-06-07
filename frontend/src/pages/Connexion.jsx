57d0dbc031ab0dc14f48c860379fc26bde32e068
Carenjot <carenjot.aurelien@hotmail.fr>
2023-06-07 17:09:10 +0200

    add some css on the connexion

--- frontend/src/pages/Connexion.jsx
+++ frontend/src/pages/Connexion.jsx
@@ -2,20 +2,25 @@ import { useState } from "react";
 import Login from "./Login";
 import Register from "./Register";
 
+import logo from "../assets/logo.png";
+
 function Connexion() {
   const [isLogin, setIsLogin] = useState(true);
 
   return (
-    <div className="form-connexion">
-      <div className="change-menu">
-        <button type="button" onClick={() => setIsLogin(true)}>
-          Login
-        </button>
-        <button type="button" onClick={() => setIsLogin(false)}>
-          Register
-        </button>
+    <div className="connexion">
+      <img src={logo} alt="" />
+      <div className="form-connexion">
+        <div className="change-menu">
+          <button type="button" onClick={() => setIsLogin(true)}>
+            Login
+          </button>
+          <button type="button" onClick={() => setIsLogin(false)}>
+            Register
+          </button>
+        </div>
+        <div className="form-content">{isLogin ? <Login /> : <Register />}</div>
       </div>
-      <div className="form-content">{isLogin ? <Login /> : <Register />}</div>
     </div>
   );
 }
