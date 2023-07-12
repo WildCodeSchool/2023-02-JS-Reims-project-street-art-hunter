896bfcc490084b265cc5a8ed5e9664534044a769
Carenjot <carenjot.aurelien@hotmail.fr>
2023-07-12 10:08:29 +0200

    add the best package ever : PIGEON MAP

--- frontend/src/components/MapStreetArt.jsx
+++ frontend/src/components/MapStreetArt.jsx
@@ -1,22 +1,14 @@
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { useParams } from "react-router-dom";
+import { Map, Marker } from "pigeon-maps";
 
 export default function MapStreetArt() {
   const { longitude, latitude } = useParams();
-  const longitudeLatitude = [longitude, latitude];
+
+  const longitudeLatitude = [parseFloat(longitude), parseFloat(latitude)];
 
   return (
-    <MapContainer
-      center={longitudeLatitude}
-      zoom={15}
-      scrollWheelZoom={false}
-      style={{ flexGrow: 1, minHeight: "initial" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={longitudeLatitude} />
-    </MapContainer>
+    <Map height={400} defaultCenter={longitudeLatitude} defaultZoom={14}>
+      <Marker width={50} anchor={longitudeLatitude} />
+    </Map>
   );
 }
