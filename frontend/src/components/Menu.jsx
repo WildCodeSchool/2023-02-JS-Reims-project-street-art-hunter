10b71accfe2fae6b197fcaba1088495ec3121da2
OkamiAka <nils.caqueux@gmail.com>
2023-07-24 12:34:53 +0200

    corection menu

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -26,7 +26,7 @@ export default function Menu() {
   const [numberY, setNumberY] = useState(1);
   const [numberX, setNumberX] = useState(1);
   const { gameBoyColor } = useAuth();
-  const sizeIconMenu = "5rem";
+  const sizeIconMenu = "60%";
   const sizeNavMenu = "2rem";
   const menu = [
     [
