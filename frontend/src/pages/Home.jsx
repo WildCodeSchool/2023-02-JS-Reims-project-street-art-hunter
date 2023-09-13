2c4f7290af032f140015d80c2caf2bad68e2fdfd
FlavienJovic <flavien.jovic@gmail.com>
2023-09-13 16:24:02 +0200

    intitial commit test

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.svg";
 export default function Home() {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="Logo" />
+      <img src={logo} className="App-logo" alt="logo" />
       <p>Hello Vite + React !</p>
 
       <Counter />
